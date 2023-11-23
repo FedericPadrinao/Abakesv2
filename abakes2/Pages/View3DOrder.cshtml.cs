4355394d5a7b806a3e522eb281d72b30493c2bca
FedericPadrinao <federicivan.padrinao.cics@ust.edu.ph>
2023-11-23 20:56:54 +0800

    Version 15

--- abakes2/Pages/View3DOrder.cshtml.cs
+++ abakes2/Pages/View3DOrder.cshtml.cs
@@ -10,6 +10,7 @@ namespace abakes2.Pages
         public OrderSimpleInfo orderSimple = new OrderSimpleInfo();
         public CustomerInfo customerInfo = new CustomerInfo();
         public Order3DForm order3d = new Order3DForm();
+        public List<Asset3DForm> listAsset3D = new List<Asset3DForm>();
         public string userconfirm = "";
         public String errorMessage = "";
         public String successMessage = "";
@@ -35,7 +36,7 @@ namespace abakes2.Pages
                             {
                                 order3d.ModelID = reader.GetInt32(0);
                                 order3d.username = reader.GetString(1);
-                                order3d.ModelName1 = reader.GetString(2);     
+                                order3d.ModelName1 = reader.GetString(2);
                                 order3d.Scale1 = reader.GetInt32(3);
                                 order3d.Texture1 = reader.GetString(4);
                                 order3d.Texture2 = reader.GetString(5);
@@ -45,6 +46,39 @@ namespace abakes2.Pages
                                 order3d.Color3 = reader.GetString(9);
 
 
+                            }
+                        }
+                    }
+
+
+                }
+
+                using (SqlConnection connection = new SqlConnection(connectionProvider))
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM Asset3DForm WHERE OrderID=@id ";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Asset3DForm asset3D = new Asset3DForm();
+                                asset3D.AssetID = reader.GetInt32(0);
+                                asset3D.OrderID = reader.GetInt32(1);
+                                asset3D.username = reader.GetString(2);
+                                asset3D.AssetName = reader.GetString(3);
+                                asset3D.AssetPath = reader.GetString(4);
+                                asset3D.AssetScale = reader.GetInt32(5);
+                                asset3D.PositionX = reader.GetString(6);
+                                asset3D.PositionY = reader.GetString(7);
+                                asset3D.PositionZ = reader.GetString(8);
+
+                                listAsset3D.Add(asset3D);
+                                Console.WriteLine("Assets" + asset3D.AssetPath);
+                                Console.WriteLine("Assets" + asset3D.AssetName);
                             }
                         }
                     }
